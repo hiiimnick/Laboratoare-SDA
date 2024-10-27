f876e444b81eaadf5fe03827dd1ff892d92bb92d
hiiimnick <95238421+hiiimnick@users.noreply.github.com>
2024-10-27 22:34:14 +0200

    Add files via upload

---
+++ Laborator2/src/clase/Profesor.java
@@ -0,0 +1,16 @@
+package clase;
+
+public class Profesor {
+    String nume;
+    String prenume;
+
+    @Override
+    public String toString() {
+        return "Profesor{" + "nume=" + nume + ", prenume=" + prenume + '}';
+    }
+
+    public Profesor(String nume, String prenume) {
+        this.nume = nume;
+        this.prenume = prenume;
+    }
+}
