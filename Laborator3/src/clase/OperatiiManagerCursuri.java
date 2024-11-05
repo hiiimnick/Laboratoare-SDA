f27e60283e8fe97aac42db86d01421db08c8ed9c
hiiimnick <95238421+hiiimnick@users.noreply.github.com>
2024-11-05 17:28:47 +0200

    Delete Laborator3/src directory

--- Laborator3/src/clase/OperatiiManagerCursuri.java
+++
@@ -1,7 +0,0 @@
-package clase;
-
-public interface OperatiiManagerCursuri {
-    public void AddCurs(Curs curs);
-    public void UpdateCurs(Curs curs, String numeNou, String descriereNoua, Profesor profNou);
-    public void DeleteCurs(Curs curs);
-}
